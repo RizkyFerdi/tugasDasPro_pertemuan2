5c16316534ecd29f57c7dfd6d365615f5c1a4783
Hamm <ilhampolpro45@gmail.com>
2024-09-07 13:42:49 +0700

    Menambahkan Identitas Ilham

---
+++ identitasMahasiswa16.java
@@ -0,0 +1,16 @@
+public class identitasMahasiswa16 {
+
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------");
+        System.out.println("---------- Identitas Diri Mahasiswa ----------");
+        System.out.println("----------------------------------------------");
+        System.out.println("--------------- Mahasiswa ke-2 ---------------");
+        System.out.println("Nama\t        : Mokhammad Ilham Putra Wijaya");
+        System.out.println("NIM\t        : 244107020139");
+        System.out.println("TTL\t        : Kota Madiun, 14 Mei 2005");
+        System.out.println("Alamat\t        : Kec. Kebonsari, Kab. Madiun");
+        System.out.println("Jenis kelamin\t: Laki-Laki");
+        System.out.println("No. HP\t        : 0895366923393");
+        System.out.println("Email\t        : mokhamm20@gmail.com");
+    }
+}
