feec414626e54b6d025c0e97002fd61fac784d1e
rsyadipras <rsyadipras@gmail.com>
2024-09-07 14:35:40 +0700

    Menambahkan identitas adi

---
+++ identitasMahasiswa22.java
@@ -0,0 +1,15 @@
+public class identitasMahasiswa22 {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------");
+        System.out.println("--------- Identitas Diri Mahasiswa ---------");
+        System.out.println("--------------------------------------------");
+        System.out.println("-------------- Mahasiswa ke-3 --------------");
+        System.out.println("Nama    \t: Risky Adi Prasetya");
+        System.out.println("NIM     \t: 244107020153");
+        System.out.println("TTL     \t: Kota Malang, 07 Juni 2005");
+        System.out.println("Alamat  \t: Kec. Blimbing, Kota Malang");
+        System.out.println("Jenis kelamin\t: Laki-Laki");
+        System.out.println("No. HP  \t: 085335098465");
+        System.out.println("Email   \t: rsyadipras@gmail.com");
+    }
+}
