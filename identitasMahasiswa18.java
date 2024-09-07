7bf1d98925463121a83b44d2e802a3afdf8b99e3
RizkyFerdi <mazrizky46@gmail.com>
2024-09-07 13:27:54 +0700

    menambahkan identitas rizky

---
+++ identitasMahasiswa18.java
@@ -0,0 +1,16 @@
+public class identitasMahasiswa18 {
+    
+    public static void main(String[] args) {
+    System.out.println("----------------------------------------------");
+    System.out.println("-----------Identitas Diri Mahasiswa-----------");
+    System.out.println("----------------------------------------------");
+    System.out.println("---------------Mahasiswa Ke-1-----------------");
+    System.out.println("Nama \t\t: Muhammad Rizky Firdiansyah");
+    System.out.println("NIM \t\t: 244107020013");
+    System.out.println("TTL \t\t: Pasuruan, 22 Januari 2006");
+    System.out.println("Alamat \t\t: Gempol Pasuruan");
+    System.out.println("Jenis Kelamin \t: Laki-laki");
+    System.out.println("No HP \t\t: 0895403239310");
+    System.out.println("Email \t\t: mazrizky46@gmail.com");
+    }
+}
